c7afa157454f949fe1972a20139d5c50396e1af8
Andrea R <amrdev08@gmail.com>
2017-07-10 14:21:55 -0400

    moved some files around

---
+++ controllers/page_controller.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const router = express.Router();
+const path = require("path");
+const Zendesk = require('zendesk-node-api');
+
+const zendesk = new Zendesk({
+  url: process.env.ZENDESK_URL, 
+  email: process.env.ZENDESK_EMAIL, 
+  token: process.env.ZENDESK_API_TOKEN 
+});
+
+// // //Landing
+router.get("/", function(req, res){
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
+router.get("/callback", function(req, res){
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
+router.get("/home", function(req, res){
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
+router.get("/home/submit", function(req, res){
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
+router.get("/home/display", function(req, res){
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+
+//Post Ticket to Zendesk
+router.post("/home/submit", function(req,res) {
+  console.log(req.body.subject)
+
+  zendesk.tickets.create({
+    subject: req.body.subject, 
+    comment: { 
+    body: req.body.body 
+  }
+  }).then(function(result){
+    return result == true 
+  })  
+});
+
+//Display tickets
+router.get("/api", function(req,res) {
+   zendesk.tickets.list().then(function(tickets){
+    res.send(tickets);
+  });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 "use strict";
 
 const express = require("express");
-const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const path = require("path");
-const Zendesk = require('zendesk-node-api');
 
 let app = express();
 
@@ -17,61 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
-const zendesk = new Zendesk({
-  url: process.env.ZENDESK_URL, 
-  email: process.env.ZENDESK_EMAIL, 
-  token: process.env.ZENDESK_API_TOKEN 
-});
-
-// // //Landing
-app.get("/", function(req, res){
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
 //bring in global css/js/html
 app.use(express.static(__dirname + "/public"))
 
 
-app.get("/callback", function(req, res){
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
-app.get("/home", function(req, res){
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
-app.get("/home/submit", function(req, res){
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
-app.get("/home/display", function(req, res){
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
-
-//Post Ticket to Zendesk
-app.post("/home/submit", function(req,res) {
-  console.log(req.body.subject)
-
-  zendesk.tickets.create({
-    subject: req.body.subject, 
-    comment: { 
-    body: req.body.body 
-  }
-  }).then(function(result){
-    console.log(result);
-    console.log("Ticket Created");
-    result == true 
-  })  
-});
-
-//Display tickets
-app.get("/api", function(req,res) {
-   zendesk.tickets.list().then(function(tickets){
-    res.send(tickets);
-    console.log("Tickets obtained from API");
-  });
-})
+var routes = require('./controllers/page_controller.js');
+app.use('/', routes);
 
 //Port Listener
 app.listen(PORT, function() {
